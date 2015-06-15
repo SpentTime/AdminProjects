9fa9a16412c39764b0055387c2697fd38e54d6a2
SpentTime <justin.sepulveda@hotmail.com>
2015-06-14 17:36:30 -0700

    Added threading to class creation

--- WMITestRun/SystemAssetInfo.cs
+++ WMITestRun/SystemAssetInfo.cs
@@ -1,52 +1,24 @@
 ﻿using System.Management;
+using System.Threading;
 using System.Collections.Generic;
 
 namespace WMITestRun // rename namespace at somepoint...
 {
     class SystemAssetInfo  // not sure if I want a class or a struct here
     {
-        private string _name;
-        private string _serial;
-        private string _asset;
-        private bool _good_connection;
-
-        public string Name
-        {
-            get
-            {
-                return this._name;
-            }
-        }
-        public string Serial
-        {
-            get
-            {
-                return this._serial;
-            }
-        }
-        public string Asset
-        {
-            get
-            {
-                return this._asset;
-            }
-        }
-        public bool GoodConnection
-        {
-            get
-            {
-                return this._good_connection;
-            }
-        }
+        public string Name { get; private set; }
+        public string Serial { get; private set; }
+        public string Asset { get; private set; }
+        public bool GoodConnection{ get; private set; }
 
         //using https://msdn.microsoft.com/en-US/library/ms257337%28v=vs.80%29.aspx for examples
         // on how to connect to WMI remotely.
         public SystemAssetInfo(string computerName)
         {
-            this._name = computerName;
-            ManagementScope scope = new ManagementScope("\\\\" + this._name + "\\root\\cimv2");
+            this.Name = computerName;
+            ManagementScope scope = new ManagementScope("\\\\" + this.Name + "\\root\\cimv2");
 
-            this._good_connection = true;
+            this.GoodConnection = true;
 
             try  // Putting this here because even if an exception happens, I want the object created.
             {
@@ -55,30 +27,30 @@ namespace WMITestRun // rename namespace at somepoint...
             }
             catch (System.Runtime.InteropServices.COMException e)
             {
-                this._good_connection = false;
-                this._asset = "Bad Connection";
-                this._serial = e.Message;
+                this.GoodConnection = false;
+                this.Asset = "Bad Connection";
+                this.Serial = e.Message;
             }
             catch (System.UnauthorizedAccessException e)
             {
-                this._good_connection = false;
-                this._asset = "Bad Login";
-                this._serial = e.Message;
+                this.GoodConnection = false;
+                this.Asset = "Bad Login";
+                this.Serial = e.Message;
             }
             catch (System.Exception e)
             {
-                this._good_connection = false;
-                this._asset = e.ToString();
-                this._serial = e.Message; 
+                this.GoodConnection = false;
+                this.Asset = e.ToString();
+                this.Serial = e.Message; 
             }
         }
 
         public SystemAssetInfo(string computerName, ConnectionOptions connectionOptions)
         {
-            this._name = computerName;
-            ManagementScope scope = new ManagementScope("\\\\" + this._name + "\\root\\cimv2", connectionOptions);
+            this.Name = computerName;
+            ManagementScope scope = new ManagementScope("\\\\" + this.Name + "\\root\\cimv2", connectionOptions);
 
-            this._good_connection = true;
+            this.GoodConnection = true;
 
             try  // Putting this here because even if an exception happens, I want the object created.
             {
@@ -87,21 +59,21 @@ namespace WMITestRun // rename namespace at somepoint...
             }
             catch (System.Runtime.InteropServices.COMException e)
             {
-                this._good_connection = false;
-                this._asset = "Bad Connection";
-                this._serial = e.Message;
+                this.GoodConnection = false;
+                this.Asset = "Bad Connection";
+                this.Serial = e.Message;
             }
             catch (System.UnauthorizedAccessException e)
             {
-                this._good_connection = false;
-                this._asset = "Bad Login";
-                this._serial = e.Message;
+                this.GoodConnection = false;
+                this.Asset = "Bad Login";
+                this.Serial = e.Message;
             }
             catch (System.Exception e)
             {
-                this._good_connection = false;
-                this._asset = e.ToString();
-                this._serial = e.Message;
+                this.GoodConnection = false;
+                this.Asset = e.ToString();
+                this.Serial = e.Message;
             }
         }
 
@@ -113,7 +85,7 @@ namespace WMITestRun // rename namespace at somepoint...
 
             foreach (ManagementObject mo in queryCollection)
             {
-                this._serial = (mo["SerialNumber"].ToString()).Trim();
+                this.Serial = (mo["SerialNumber"].ToString()).Trim();
             }
         }
 
@@ -125,28 +97,33 @@ namespace WMITestRun // rename namespace at somepoint...
 
             foreach (ManagementObject mo in queryCollection)
             {
-                this._asset = (mo["SMBIOSAssetTag"].ToString()).Trim();
+                this.Asset = (mo["SMBIOSAssetTag"].ToString()).Trim();
             }
         }
 
-        // may want to implement threading with this method at some point...
         static public List<SystemAssetInfo> CreateSAIList(string[] computerNames)
         {
             List<SystemAssetInfo> saiList = new List<SystemAssetInfo>();
+            List<Thread> tList = new List<Thread>();
             foreach (string computerName in computerNames)
             {
-                saiList.Add(new SystemAssetInfo(computerName));
+                tList.Add(new Thread(() => saiList.Add(new SystemAssetInfo(computerName))));
+                tList[tList.Count - 1].Start();
             }
+            foreach (var thread in tList) { thread.Join(); }
             return saiList;
         }
 
         static public List<SystemAssetInfo> CreateSAIList(string[] computerNames, ConnectionOptions connectionOptions)
         {
             List<SystemAssetInfo> saiList = new List<SystemAssetInfo>();
+            List<Thread> tList = new List<Thread>();
             foreach (string computerName in computerNames)
             {
-                saiList.Add(new SystemAssetInfo(computerName, connectionOptions));
+                tList.Add(new Thread(() => saiList.Add(new SystemAssetInfo(computerName, connectionOptions))));
+                tList[tList.Count - 1].Start();
             }
+            foreach (var thread in tList) { thread.Join(); }
             return saiList;
         }
     }
