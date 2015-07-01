7210c76721d6177d0ba0479e5f648a51510b4ef5
SpentTime <justin.sepulveda@hotmail.com>
2015-07-01 11:21:33 -0700

    changing password input

--- WMITestRun/Program.cs
+++ WMITestRun/Program.cs
@@ -10,10 +10,9 @@ namespace WMITestRun
         {
             ConnectionOptions co = new ConnectionOptions();
             Console.WriteLine("Username :");
-            co.Username = Console.ReadLine();  
+            co.Username = Console.ReadLine();
             Console.Write("Password: ");
-            co.Password = Console.ReadLine(); /*not the most secure way to get a password, 
-                                                 but I don't plan on keeping this a console application */
+            co.Password = silientInput();
 
             List<SystemAssetInfo> sysAssetList = SystemAssetInfo.CreateSAIList(args, co);
             sysAssetList.Sort();
@@ -22,6 +21,24 @@ namespace WMITestRun
                 Console.WriteLine("{0} {1} {2}", sysAssetObj.Name, sysAssetObj.Serial, sysAssetObj.Asset);
             }
             Console.ReadKey();
+        }
+
+        static private string silientInput()
+        {
+            string temp = "";
+            ConsoleKeyInfo ckinfo = Console.ReadKey(true);
+
+            while (ckinfo.Key != ConsoleKey.Enter)
+            {
+                if (ckinfo.Key == ConsoleKey.Backspace && temp.Length > 0)
+                    temp.Remove(temp.Length - 1);
+                else if (ckinfo.KeyChar > 31 && ckinfo.KeyChar < 127)
+                    temp += ckinfo.KeyChar;
+
+                ckinfo = Console.ReadKey(true);
+            }
+
+            return temp;
         }
     }
 }
