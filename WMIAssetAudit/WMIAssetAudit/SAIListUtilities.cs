a7eb0affa1483ec6e3085c163c2a84b8ba58f005
SpentTime <justin.sepulveda@hotmail.com>
2015-07-04 15:04:47 -0700

    removing unused usings

--- WMIAssetAudit/WMIAssetAudit/SAIListUtilities.cs
+++ WMIAssetAudit/WMIAssetAudit/SAIListUtilities.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Management;
-using System.Data;
 
 namespace QDAudit
 {
