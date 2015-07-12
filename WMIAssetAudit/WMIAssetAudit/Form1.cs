da09d96ff7128b8830adc36f7565582f461fdaea
SpentTime <justin.sepulveda@hotmail.com>
2015-07-12 15:31:44 -0700

    Revert "downloading"
    This reverts commit 41b3ff582d7d0679ac7fe70ce1e48c12765e58cc.

---
+++ WMIAssetAudit/WMIAssetAudit/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace QDAudit
+{
+    public partial class Form1 : Form
+    {
+        private DataTable _table;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this._table = new DataTable();
+            this._table.Columns.Add("Name", typeof(string));
+            this._table.Columns.Add("Asset", typeof(string));
+            this._table.Columns.Add("Serial", typeof(string));
+            this.dataGridView1.DataSource = this._table;
+        }
+
+        private void importToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string[] computerNames = Utilities.GetNamesFromFile(openFileDialog1.FileName);
+                List<SystemAssetInfo> saiList = SystemAssetInfo.CreateSAIList(computerNames);
+                Utilities.ConvertSAIListToDataSource(saiList, this._table);
+            }
+
+        }
+        
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void gatherToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            List<string> computerNames = new List<string>();
+            foreach (DataRow row in this._table.Rows)
+            {
+                computerNames.Add(row.Field<string>(0));
+            }
+            List<SystemAssetInfo> saiList = SystemAssetInfo.CreateSAIList(computerNames.ToArray());
+            this._table.Rows.Clear();
+            Utilities.ConvertSAIListToDataSource(saiList, this._table);
+        }
+    }
+}
---
+++ WMIAssetAudit/WMIAssetAudit/SAIListUtilities.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Management;
+
+namespace QDAudit
+{
+    // this is for utilities for working with type SystemAssetInfo.  Hence the use of static methods.
+    partial class SystemAssetInfo : IComparable<SystemAssetInfo>
+    {
+        static public List<SystemAssetInfo> CreateSAIList(string[] computerNames)
+        {
+            List<SystemAssetInfo> saiList = new List<SystemAssetInfo>();
+            List<Thread> tList = new List<Thread>();
+            foreach (string computerName in computerNames)
+            {
+                tList.Add(new Thread(() => saiList.Add(new SystemAssetInfo(computerName))));
+                tList[tList.Count - 1].Start();
+            }
+            foreach (var thread in tList) { thread.Join(); }
+            return saiList;
+        }
+
+        static public List<SystemAssetInfo> CreateSAIList(string[] computerNames, ConnectionOptions connectionOptions)
+        {
+            List<SystemAssetInfo> saiList = new List<SystemAssetInfo>();
+            List<Thread> tList = new List<Thread>();
+            foreach (string computerName in computerNames)
+            {
+                tList.Add(new Thread(() => saiList.Add(new SystemAssetInfo(computerName, connectionOptions))));
+                tList[tList.Count - 1].Start();
+            }
+            foreach (var thread in tList) { thread.Join(); }
+            return saiList;
+        }
+    }
+}
